13604a8cfbc0d9c54f010dafc71cf954e1e86be7
Wisebrain <BIGBROTHER>
2024-03-23 13:43:35 +0300

    feat: Создано MVC приложение

---
+++ DepartmentsWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DepartmentsWeb.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
