2a9c432a1e9a9b994e53d178c1b1da200abfbd08
Wisebrain <BIGBROTHER>
2024-03-23 17:00:27 +0300

    refactor: Изменен метод отправики сообщений в API

--- DepartmentsWeb/Services/DepartmentsService.cs
+++ DepartmentsWeb/Services/DepartmentsService.cs
@@ -1,57 +1,103 @@
 ﻿using DepartmentsWeb.Models.Dto;
 using Newtonsoft.Json;
+using System;
+using System.Text;
 
 namespace DepartmentsWeb.Services
 {
 	public class DepartmentsService : IDepartmentsService
 	{
 		private readonly IHttpClientFactory clientFactory;
+        //ToDo вынести в конфиг
+        private string Url = "http://localhost:5105/departments";
 
-		public DepartmentsService(IHttpClientFactory clientFactory)
+        public DepartmentsService(IHttpClientFactory clientFactory)
 		{
 			this.clientFactory = clientFactory ?? throw new ArgumentNullException(nameof(clientFactory));
 		}
 
 		public async Task<List<DepartmentDto>> GetFromApiAsync()
 		{
-			//ToDo вынести в конфиг
-			string Url = "http://localhost:5105/departments";
+            ResponseDto apiResponceDto = await CallApi(HttpMethod.Get);
 
-			//ToDo вынести в отдельный класс
-			try
-			{
-				//Создаем клиент запроса к сервису
-				HttpClient client = clientFactory.CreateClient("DepartmentsAPI");
+            if(apiResponceDto.IsSuccess) 
+            {
+                List<DepartmentDto> departments = JsonConvert.DeserializeObject<List<DepartmentDto>>(Convert.ToString(apiResponceDto.Result));
+                return departments;
+            }
 
-				HttpRequestMessage message = new HttpRequestMessage();
-				message.Headers.Add("Accept", "application/json");
-				//ToDo вынести в конфиг
-				message.RequestUri = new Uri(Url);
-				message.Method = HttpMethod.Get;
-
-				HttpResponseMessage apiResponse = await client.SendAsync(message);
-
-				var apiContent = await apiResponse.Content.ReadAsStringAsync();
-				var apiResponceDto = JsonConvert.DeserializeObject<ResponseDto>(apiContent);
-				var departments = JsonConvert.DeserializeObject<List<DepartmentDto>>(Convert.ToString(apiResponceDto.Result));
-				return departments;
-			}
-			catch (Exception ex) 
-			{
-				return new List<DepartmentDto>();
-			}			
+		    return new List<DepartmentDto>();		
 		}
 
 		public async Task<bool> SynchronizeDb(Stream fileStream)
 		{
-			using (StreamReader reader = new StreamReader(fileStream))
-			{ 
-				string json = reader.ReadToEnd();
-				List<DepartmentDto> departments = JsonConvert.DeserializeObject<List<DepartmentDto>>(json);
-				return true;
-			}
-
-			return false;
+            List<DepartmentDto> departments = ReadFile(fileStream);
+            if (departments.Count > 0) 
+            {
+                ResponseDto response = await CallApi(HttpMethod.Post, departments);
+                return response.IsSuccess;
+            }            
+            return false;
 		}
+
+        private List<DepartmentDto> ReadFile(Stream fileStream)
+        {
+            try
+            {
+                using (StreamReader reader = new StreamReader(fileStream))
+                {
+                    string json = reader.ReadToEnd();
+                    List<DepartmentDto> departments = JsonConvert.DeserializeObject<List<DepartmentDto>>(json);
+
+                    return departments;
+                }
+            }
+            catch
+            {
+                return new List<DepartmentDto>();
+            }
+            
+        }
+
+		private async Task<ResponseDto> CallApi(HttpMethod method, object? data = null)
+		{
+			try
+			{
+                //Создаем клиент запроса к сервису
+                HttpClient client = clientFactory.CreateClient("DepartmentsAPI");
+
+                // Формируем запрос
+                HttpRequestMessage message = new HttpRequestMessage();
+
+                message.Headers.Add("Accept", "application/json");
+                message.RequestUri = new Uri(Url);
+                message.Method = method;
+                if (data != null)
+                {
+                    message.Content = new StringContent(
+                        JsonConvert.SerializeObject(data),
+                        Encoding.UTF8,
+                        "application/json"
+                    );
+                }
+
+                // Получаем ответ
+                HttpResponseMessage apiResponse = await client.SendAsync(message);
+
+                string apiContent = await apiResponse.Content.ReadAsStringAsync();
+                ResponseDto apiResponceDto = JsonConvert.DeserializeObject<ResponseDto>(apiContent);
+
+                return apiResponceDto;
+            }
+            catch (Exception ex) 
+			{ 
+				return new ResponseDto()
+                {
+                    DisplayMessage = "Error",
+                    ErrorMessages = new List<string> { Convert.ToString(ex.Message) },
+                    IsSuccess = false
+                };
+			}
+        }
     }
 }
